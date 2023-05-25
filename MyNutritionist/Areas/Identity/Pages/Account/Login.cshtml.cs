af6ecfc07e679f275b11422e767cf4ca500fbe11
Amina Bajrić <115504860+abajric2@users.noreply.github.com>
2023-05-25 17:50:25 +0200

    login view

--- MyNutritionist/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ MyNutritionist/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -60,6 +60,9 @@ namespace MyNutritionist.Areas.Identity.Pages.Account
         /// </summary>
         public class InputModel
         {
+            [Required]
+            [PersonalData]
+            public string Username { get; set; }
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
