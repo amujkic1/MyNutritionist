46d1d2aa030e86ceb82ae9f83bcd02170e19320c
Amina Bajrić <115504860+abajric2@users.noreply.github.com>
2023-06-01 22:15:42 +0200

    constraints for input in registrationform

--- MyNutritionist/Models/Person.cs
+++ MyNutritionist/Models/Person.cs
@@ -7,9 +7,17 @@ namespace MyNutritionist.Models
     {
         [System.ComponentModel.DataAnnotations.Key]
         public int PID { get; set; }
+        [Required(ErrorMessage = "Invalid input")]
+        [RegularExpression(@"^[A-Za-z\s]+$", ErrorMessage = "The City field can only contain letters.")]
         public string Name { get; set; }
+        [EmailAddress(ErrorMessage = "Invalid input")]
         public string Email { get; set; }
+        [Required(ErrorMessage = "Invalid input")]
         public string Username { get; set; }
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Invalid input")]
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*\W).*$", ErrorMessage = "The Password field must contain at least one uppercase letter and one non-letter character.")]
+        [StringLength(100, MinimumLength = 8, ErrorMessage = "The Password field must be between 8 and 100 characters long.")]
         public string Password { get; set; }
             
         public Person() { }
--- MyNutritionist/Models/RegisteredUser.cs
+++ MyNutritionist/Models/RegisteredUser.cs
@@ -1,8 +1,12 @@
-﻿namespace MyNutritionist.Models
+﻿using System.ComponentModel.DataAnnotations;
+namespace MyNutritionist.Models
 {
     public class RegisteredUser: Person
     {
+        [RegularExpression(@"^[A-Za-z\s]+$", ErrorMessage = "Invalid input")]
+
         public string City { get; set; }
+        [StringLength(100, MinimumLength = 6, ErrorMessage = "Invalid input")]
         public int Age { get; set; }
         public double Weight { get; set; }
         public double Height { get; set; }
