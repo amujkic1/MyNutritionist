a9fd7435b801c1359f829644ed2f8f28619bfaf1
amujkic1 <amujkic1@etf.unsa.ba>
2023-06-10 15:56:06 +0200

    Small changes

--- MyNutritionist/Controllers/AdminController.cs
+++ MyNutritionist/Controllers/AdminController.cs
@@ -33,16 +33,141 @@ namespace MyNutritionist.Controllers
         // GET: Admin
         public async Task<IActionResult> Index()
         {
-            //var registeredUsers = _userManager.GetUsersInRoleAsync("RegisteredUser").Result;
-            //var idsOfRegisteredUsers = registeredUsers.Select(u => u.Id);
-            //var users = registeredUsers.OfType<ApplicationUser>();
-            return _context.Admin != null ?
-                        View(await _context.Admin.ToListAsync()) :
-                        Problem("Entity set 'ApplicationDbContext.RegisteredUser'  is null.");
+            var registeredUsers = _userManager.GetUsersInRoleAsync("RegisteredUser").Result;
+            var idsOfRegisteredUsers = registeredUsers.Select(u => u.Id);
+            var users = registeredUsers.OfType<ApplicationUser>();
 
-            //return View(user);
+
+            return View(users);
         }
-    }
+
+        // GET: Admin/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Admin == null)
+            {
+                return NotFound();
+            }
+
+            var admin = await _context.Admin
+                .FirstOrDefaultAsync(m => m.Id.Equals(id));
+            if (admin == null)
+            {
+                return NotFound();
+            }
+
+            return View(admin);
+        }
+
+        // GET: Admin/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("PID,Name,Email,Username,Password")] Admin admin)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(admin);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(admin);
+        }
+
+        // GET: Admin/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Admin == null)
+            {
+                return NotFound();
+            }
+
+            var admin = await _context.Admin.FindAsync(id);
+            if (admin == null)
+            {
+                return NotFound();
+            }
+            return View(admin);
+        }
+
+        // POST: Admin/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, [Bind("PID,Name,Email,Username,Password")] Admin admin)
+        {
+            if (id != admin.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(admin);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!AdminExists(admin.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(admin);
+        }
+
+        // GET: Admin/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Admin == null)
+            {
+                return NotFound();
+            }
+
+            var admin = await _context.Admin
+                .FirstOrDefaultAsync(m => m.Id.Equals(id));
+            if (admin == null)
+            {
+                return NotFound();
+            }
+
+            return View(admin);
+        }
+
+        // POST: Admin/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Admin == null)
+            {
+                return Problem("Entity set 'ApplicationDbContext.Admin'  is null.");
+            }
+            var admin = await _context.Admin.FindAsync(id);
+            if (admin != null)
+            {
+                _context.Admin.Remove(admin);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
 
         public async Task<IActionResult> UpgradeToPremium(string regName)
         {
@@ -57,24 +182,11 @@ namespace MyNutritionist.Controllers
                 return NotFound("Registered user not found.");
             }
 
-            try {
-                var premiumUser = Activator.CreateInstance<PremiumUser>();
 
-                premiumUser.AccountNumber = "";
-                premiumUser.City = registeredUser.City;
-                premiumUser.Height = registeredUser.Height;
-                premiumUser.Weight = registeredUser.Weight;
-                premiumUser.Age = registeredUser.Age;
-                premiumUser.Points = 0;
-                premiumUser.AspUserId = 0;
-
-                _context.PremiumUser.Add(premiumUser);
-                await _userManager.RemoveFromRoleAsync(registeredUser, "RegisteredUser");
-                await _userManager.AddToRoleAsync(registeredUser, "PremiumUser");
-
-                await _context.SaveChangesAsync();
-
-                return RedirectToAction(nameof(Index));
+            var premiumUser = new PremiumUser(); 
+            try
+            {
+                premiumUser = Activator.CreateInstance<PremiumUser>();
             }
             catch
             {
@@ -83,14 +195,62 @@ namespace MyNutritionist.Controllers
                     $"override the register page in /Areas/Identity/Pages/Account/Register.cshtml");
             }
 
+            premiumUser.AccountNumber = "";
+                premiumUser.City = registeredUser.City;
+                premiumUser.Height = registeredUser.Height;
+                premiumUser.Weight = registeredUser.Weight;
+                premiumUser.Age = registeredUser.Age;
+                premiumUser.Points = 0;
+                premiumUser.AspUserId = 0;
+
+                _context.PremiumUser.Add(premiumUser);
+
+                await _userManager.RemoveFromRoleAsync(registeredUser, "RegisteredUser");
+                await _userManager.AddToRoleAsync(registeredUser, "PremiumUser");
+
+                await _context.SaveChangesAsync();
+
+                var nutritionist = await _context.Nutritionist
+               .FirstOrDefaultAsync(m => m.NutriUsername.Equals("nutri123"));
+                if (nutritionist == null)
+                {
+                    return NotFound();
+                }
+
+                nutritionist.PremiumUsers.Add(await _context.PremiumUser
+                .FirstOrDefaultAsync(m => m.Id.Equals(registeredUser.Id)));
+
+                await _context.SaveChangesAsync();
+
+                return RedirectToAction(nameof(Index));
+            
+            
+
+        }
+
+        public async Task<IActionResult> AssignNutritionist(int regId)
+        {
+            var nutritionist = await _context.Nutritionist
+               .FirstOrDefaultAsync(m => m.NutriUsername.Equals("nutri123"));
+            if (nutritionist == null)
+            {
+                return NotFound();
+            }
+
+            nutritionist.PremiumUsers.Add(await _context.PremiumUser
+            .FirstOrDefaultAsync(m => m.Id.Equals(regId)));
+
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
         }
 
         private bool AdminExists(string id)
         {
           return (_context.Admin?.Any(e => e.Id.Equals(id))).GetValueOrDefault();
+
         }
     }
-    
 
 }
 
