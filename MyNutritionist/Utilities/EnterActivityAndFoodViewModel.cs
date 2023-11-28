3df362de9922a7e260f0888337d6a7d85e58c649
Esma <ebajramovi3@etf.unsa.ba>
2023-11-28 08:40:05 +0100

    enter daily intake changed

--- MyNutritionist/Utilities/EnterActivityAndFoodViewModel.cs
+++ MyNutritionist/Utilities/EnterActivityAndFoodViewModel.cs
@@ -4,9 +4,13 @@
     {
         public List<Ingredient> Ingredients { get; set; }
         public Ingredient Breakfast { get; set; }
+        public int BreakfastQuantity { get; set; }
         public Ingredient Lunch { get; set; }
+        public int LunchQuantity { get; set; }
         public Ingredient  Dinner { get; set; }
+        public int DinnerQuantity { get; set; }
         public Ingredient Snacks { get; set; }
+        public int SnacksQuantity { get; set; }
 
         public PhysicalActivity PhysicalActivity { get; set; }
 
