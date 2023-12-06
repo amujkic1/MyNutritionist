62765ca339048813a517d3f04df218ca8b275f6f
Edna1010 <ebasic1@etf.unsa.ba>
2023-12-06 18:22:09 +0100

    Added comments

--- MyNutritionist/Utilities/ISubject.cs
+++ MyNutritionist/Utilities/ISubject.cs
@@ -1,7 +1,12 @@
 ﻿namespace MyNutritionist.Utilities
 {
+    /// Interface for subjects that support login functionality.
     public interface ISubject
     {
+        /// Attempts to authenticate a user based on the provided username and password.
+        /// "username" the username of the user attempting to log in.
+        /// "password" the password of the user attempting to log in.
+        /// returns ztrue if the login is successful; otherwise, false.
         bool Login(string username, string password);
     }
 }
--- MyNutritionist/Utilities/Iterator.cs
+++ MyNutritionist/Utilities/Iterator.cs
@@ -1,10 +1,17 @@
 ﻿using MyNutritionist.Models;
+using NuGet.Packaging.Signing;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace MyNutritionist.Utilities
 {
+    /// Interface for an iterator over a collection of Recipe objects.
     public interface Iterator
     {
+        ///Finds and returns the next Recipe in the iteration sequence.
+        /// returns the next Recipe in the iteration sequence.
         public Recipe findNextRecipe();
+        /// Gets the current Recipe in the iteration.
+        /// returns the current Recipe in the iteration.
         public Recipe getRecipe();
     }
 }
