ee6c06884d73fb29747ce1f486aa64d22adeb849
BenjaminKadic <bkadic1@etf.unsa.ba>
2023-12-07 09:19:53 +0100

    dodani komentari

--- MyNutritionist/Utilities/ProtectionProxy.cs
+++ MyNutritionist/Utilities/ProtectionProxy.cs
@@ -1,32 +1,62 @@
 ﻿namespace MyNutritionist.Utilities
 {
+    /*
+     * ProtectionProxy klasa predstavlja zamjenski objekat (proxy) koji štiti stvarni subjekat 
+     * (RealSubject) od neovlaštenih pristupa.
+     * 
+     * @implements ISubject interfejs.
+     */
     public class ProtectionProxy : ISubject
     {
-            private readonly RealSubject _realSubject;
+        private readonly RealSubject _realSubject;
 
-            public ProtectionProxy()
+        /*
+         * Konstruktor ProtectionProxy klase.
+         * Inicijalizira stvarni subjekat (RealSubject).
+         */
+        public ProtectionProxy()
+        {
+            _realSubject = new RealSubject();
+        }
+
+        /*
+         * Metoda za prijavu korisnika sa provjerom permisija.
+         *
+         * @param username: Korisničko ime.
+         * @param password: Šifra korisnika.
+         *
+         * @return: True ako je prijava uspješna, inače false.
+         */
+        public bool Login(string username, string password)
+        {
+            if (ValidateRequest(username, password))
             {
-                _realSubject = new RealSubject();
+                return _realSubject.Login(username, password);
             }
 
-            public bool Login(string username, string password)
-            {
-                if (ValidateRequest(username, password))
-                {
-                    return _realSubject.Login(username, password);
-                }
+            return false;
+        }
 
-                
+        /*
+         * Privatna metoda za validaciju korisničkih podataka prije prijave.
+         *
+         * @param username: Korisničko ime.
+         * @param password: Šifra korisnika.
+         *
+         * @return: True ako su podaci validni, inače false.
+         */
+        private bool ValidateRequest(string username, string password)
+        {
+            /*
+             * Provjera da li su korisničko ime i šifra postavljeni.
+             */
+            if (username == null || password == null)
+            {
                 return false;
             }
 
-            private bool ValidateRequest(string username, string password)
-            {
-                if(username == null || password == null){
-                return false;
-            }
-
+           
             return true;
-            }
+        }
     }
 }
