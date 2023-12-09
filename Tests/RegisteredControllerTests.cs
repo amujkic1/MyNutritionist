263596c2f7e990571b0b9eda0f848ee931003538
Amna Hodžić <ahodzic1@etf.unsa.ba>
2023-12-09 18:25:57 +0100

    added tests for editcard and nutritionalvalues

--- Tests/RegisteredControllerTests.cs
+++ Tests/RegisteredControllerTests.cs
@@ -173,6 +173,77 @@ namespace Tests
             Assert.AreEqual(25, updatedUser.Age);
         }
 
+        [TestMethod]
+        public async Task EditCard_WhenRegisteredUserIsNull_ReturnsNotFound()
+        {
+            var registeredUserList = new List<RegisteredUser>
+            {
+                new RegisteredUser { Id = "userId"},
+             };
+            _mockUserManager.Setup(um => um.GetUserId(_mockHttpContextAccessor.Object.HttpContext.User)).Returns("userId1");
+
+            _mockDbContext.Setup(db => db.RegisteredUser).ReturnsDbSet(registeredUserList);
+
+
+            // Act
+            var result = await _controller.EditCard();
+
+            Assert.IsInstanceOfType(result, typeof(NotFoundResult));
+        }
+
+        [TestMethod]
+        public async Task EditCard_WhenRegisteredUserIsNotNull_ReturnsView()
+        {
+            var registeredUserList = new List<RegisteredUser>
+            {
+                new RegisteredUser { Id = "userId"},
+             };
+            _mockUserManager.Setup(um => um.GetUserId(_mockHttpContextAccessor.Object.HttpContext.User)).Returns("userId");
+
+            _mockDbContext.Setup(db => db.RegisteredUser).ReturnsDbSet(registeredUserList);
+
+
+            // Act
+            var result = await _controller.EditCard();
+
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+
+        [TestMethod]
+        public void NutritionalValues_ReturnsViewWithIngredients()
+        {
+            // Arrange
+            var ingredientList = new List<Ingredient>
+    {
+        new Ingredient { IId = 1, FoodName = "Ingredient1", Calories = 100, Carbs = 20, Protein = 10, Sugar = 5, Fat = 8, SaturatedFat = 3, VitaminA = 50, VitaminC = 30, Calcium = 15, Iron = 2, Sodium = 100 },
+        new Ingredient { IId = 2, FoodName = "Ingredient2", Calories = 150, Carbs = 30, Protein = 15, Sugar = 7, Fat = 10, SaturatedFat = 4, VitaminA = 70, VitaminC = 40, Calcium = 20, Iron = 3, Sodium = 120 },
+        // Add more ingredients...
+    };
+
+            _mockDbContext.Setup(db => db.Ingredient).ReturnsDbSet(ingredientList);
+
+            // Act
+            var result = _controller.NutritionalValues();
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+
+            if (result is ViewResult viewResult)
+            {
+                // Assert that the model passed to the view is a list of ingredients
+                Assert.IsInstanceOfType(viewResult.Model, typeof(List<Ingredient>));
+
+                var model = viewResult.Model as List<Ingredient>;
+
+                // Add more specific assertions based on your expectations
+                Assert.AreEqual(ingredientList.Count, model.Count);
+
+                // Example: Assert that the first ingredient's name is as expected
+                Assert.AreEqual("Ingredient1", model[0].FoodName);
+            }
+           
+        }
+
     }
 
 }
