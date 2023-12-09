22bc1932d6b10eceeec007d1bb93f68f7ff5d53e
Amna Hodžić <ahodzic1@etf.unsa.ba>
2023-12-09 17:57:23 +0100

    added tests for edit in registeredUserController

--- Tests/RegisteredControllerTests.cs
+++ Tests/RegisteredControllerTests.cs
@@ -36,7 +36,7 @@ namespace Tests
 
             _mockDbContext = new Mock<ApplicationDbContext>(options);
             _mockUserManager = new Mock<UserManager<ApplicationUser>>(new Mock<IUserStore<ApplicationUser>>().Object, null, null, null, null, null, null, null, null);
-          
+
             _mockHttpContextAccessor = new Mock<IHttpContextAccessor>();
 
             // Set up HttpContext User
@@ -67,13 +67,112 @@ namespace Tests
             _mockUserManager.Setup(um => um.GetUserId(_mockHttpContextAccessor.Object.HttpContext.User)).Returns("userId1");
 
             _mockDbContext.Setup(db => db.RegisteredUser).ReturnsDbSet(registeredUserList);
-            
+
 
             // Act
             var result = await _controller.Delete();
 
             Assert.IsInstanceOfType(result, typeof(NotFoundResult));
         }
+
+        [TestMethod]
+        public async Task Delete_WhenRegisteredUserIsNotNull_ReturnsView()
+        {
+            var registeredUserList = new List<RegisteredUser>
+            {
+                new RegisteredUser { Id = "userId"},
+             };
+            _mockUserManager.Setup(um => um.GetUserId(_mockHttpContextAccessor.Object.HttpContext.User)).Returns("userId");
+
+            _mockDbContext.Setup(db => db.RegisteredUser).ReturnsDbSet(registeredUserList);
+
+
+            // Act
+            var result = await _controller.Delete();
+
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+
+        [TestMethod]
+        public async Task Edit_WhenRegisteredUserIsNull_ReturnsNotFound()
+        {
+            var registeredUserList = new List<RegisteredUser>
+            {
+                new RegisteredUser { Id = "userId"},
+             };
+            _mockUserManager.Setup(um => um.GetUserId(_mockHttpContextAccessor.Object.HttpContext.User)).Returns("userId1");
+
+            _mockDbContext.Setup(db => db.RegisteredUser).ReturnsDbSet(registeredUserList);
+
+
+            // Act
+            var result = await _controller.Edit();
+
+            Assert.IsInstanceOfType(result, typeof(NotFoundResult));
+        }
+
+        [TestMethod]
+        public async Task Edit_WhenRegisteredUserIsNotNull_ReturnsView()
+        {
+            var registeredUserList = new List<RegisteredUser>
+            {
+                new RegisteredUser { Id = "userId"},
+             };
+            _mockUserManager.Setup(um => um.GetUserId(_mockHttpContextAccessor.Object.HttpContext.User)).Returns("userId");
+
+            _mockDbContext.Setup(db => db.RegisteredUser).ReturnsDbSet(registeredUserList);
+
+
+            // Act
+            var result = await _controller.Edit();
+
+            Assert.IsInstanceOfType(result, typeof(ViewResult));
+        }
+
+        // ... (existing code)
+
+        [TestMethod]
+        public async Task Edit_WhenRegisteredUserIsNotNull_UpdatesUserAndRedirectsToIndex()
+        {
+            // Arrange
+            var userId = "userId";
+            var registeredUserList = new List<RegisteredUser>
+            {
+                  new RegisteredUser { Id = userId },
+            };
+            _mockUserManager.Setup(um => um.GetUserId(_mockHttpContextAccessor.Object.HttpContext.User)).Returns(userId);
+            _mockDbContext.Setup(db => db.RegisteredUser).ReturnsDbSet(registeredUserList);
+
+            // Act
+            var result = await _controller.Edit(new RegisteredUser
+            {
+                FullName = "New FullName",
+                Email = "newemail@example.com",
+                NutriUsername = "newNutriUsername",
+                NutriPassword = "newPassword",
+                Weight = 70,
+                Height = 180,
+                City = "New City",
+                Age = 25
+            });
+
+            // Assert
+            Assert.IsInstanceOfType(result, typeof(RedirectToActionResult));
+            Assert.AreEqual("Index", (result as RedirectToActionResult).ActionName);
+
+            // Verify that the user's information was updated
+            var updatedUser = _mockDbContext.Object.RegisteredUser.SingleOrDefault(u => u.Id == userId);
+            Assert.IsNotNull(updatedUser);
+            Assert.AreEqual("New FullName", updatedUser.FullName);
+            Assert.AreEqual("newemail@example.com", updatedUser.Email);
+            Assert.AreEqual("newNutriUsername", updatedUser.NutriUsername);
+            Assert.AreEqual("newPassword", updatedUser.NutriPassword);
+            Assert.AreEqual(70, updatedUser.Weight);
+            Assert.AreEqual(180, updatedUser.Height);
+            Assert.AreEqual("New City", updatedUser.City);
+            Assert.AreEqual(25, updatedUser.Age);
+        }
+
     }
 
 }
