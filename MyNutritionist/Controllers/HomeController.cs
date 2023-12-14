59b2680cc3b32329d509bb414e0f0e8e759179ae
Esma <ebajramovi3@etf.unsa.ba>
2023-12-14 21:52:36 +0100

    green phase for TrainingTest

--- MyNutritionist/Controllers/HomeController.cs
+++ MyNutritionist/Controllers/HomeController.cs
@@ -60,14 +60,7 @@ namespace MyNutritionist.Controllers
 
         public async Task<IActionResult> Training()
         {
-            var trainings = new List<Training> {
-                new Training { TID = 1, nameOfTraining="Training", image="image.png", link="", duration = 10},
-                new Training { TID = 2, nameOfTraining="Training", image="image.png", link="", duration = 40},
-                new Training { TID = 3, nameOfTraining="Training", image="image.png", link="", duration = 30},
-                new Training { TID = 4, nameOfTraining="Training", image="image.png", link="", duration = 20},
-                new Training { TID = 5, nameOfTraining="Training", image="image.png", link="", duration = 10},
-                new Training { TID = 6, nameOfTraining="Training", image="image.png", link="", duration = 20}
-            };
+            var trainings = _context.Training.ToList();
             return View(trainings);
         }
     }
