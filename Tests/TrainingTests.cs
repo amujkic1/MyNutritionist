02442da2fbd895e54befd17fd9b6f1b057e678e9
Esma <ebajramovi3@etf.unsa.ba>
2023-12-14 20:44:23 +0100

    TDD

--- Tests/TrainingTests.cs
+++ Tests/TrainingTests.cs
@@ -19,27 +19,6 @@ namespace MyNutritionist.Controllers.Tests
         [TestMethod]
         public async Task Training_ReturnsCorrectModel()
         {
-            var _mockDbContext = new Mock<ApplicationDbContext>();
-            var _mockLogger = new Mock<ILogger<HomeController>>();
-            var _controller = new HomeController(_mockLogger.Object, _mockDbContext.Object);
-
-            var trainings = new List<Training> {
-                new Training { TID = 1, nameOfTraining="Training", image="image.png", link="", duration = 10},
-                new Training { TID = 2, nameOfTraining="Training", image="image.png", link="", duration = 40},
-                new Training { TID = 3, nameOfTraining="Training", image="image.png", link="", duration = 30},
-                new Training { TID = 4, nameOfTraining="Training", image="image.png", link="", duration = 20},
-                new Training { TID = 5, nameOfTraining="Training", image="image.png", link="", duration = 10},
-                new Training { TID = 6, nameOfTraining="Training", image="image.png", link="", duration = 20}
-            };
-
-            _mockDbContext.Setup(db => db.Training).ReturnsDbSet(trainings);
-
-            var result = await _controller.Training() as ViewResult;
-
-            Assert.IsNotNull(result);
-            var trainingList = result.Model as List<Training>;
-            Assert.IsNotNull(trainingList);
-            Assert.AreEqual(0, trainingList.Count);
         }
     }
 }
