874e54a3f66db33bfae88684d1d2cd672eb58560
Esma <ebajramovi3@etf.unsa.ba>
2023-12-17 01:02:41 +0100

    add whitebox tests

---
+++ Tests/White Box Tests/ISortWB.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Routing;
+using MyNutritionist.Models;
+using MyNutritionist.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests.White_Box_Tests
+{
+    [TestClass()]
+    public class ISortWB
+    {
+        [TestMethod]
+        public void Sort_NullInput_ReturnsEmptyList()
+        {
+            // Arrange
+            var sorter = new ISort();
+
+            // Act
+            List<PremiumUser> result = sorter.Sort(null, (x, y) => x.Id.CompareTo(y.Id));
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(0, result.Count);
+        }
+
+        [TestMethod]
+        public void Sort_EmptyList_ReturnsSameList()
+        {
+            // Arrange
+            var sorter = new ISort();
+            List<PremiumUser> users = new List<PremiumUser>();
+
+            // Act
+            List<PremiumUser> result = sorter.Sort(users, (x, y) => x.Id.CompareTo(y.Id));
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreEqual(users.Count, result.Count);
+        }
+
+        [TestMethod]
+        public void Sort_SortsUsersById()
+        {
+            // Arrange
+            var sorter = new ISort();
+            List<PremiumUser> users = new List<PremiumUser>
+        {
+            new PremiumUser { Weight = 70 },
+            new PremiumUser { Weight = 63 },
+            new PremiumUser { Weight = 95 }
+        };
+
+            // Act
+            List<PremiumUser> result = sorter.Sort(users, (x, y) => x.Weight.CompareTo(y.Weight));
+
+            // Assert
+            Assert.AreEqual(63, result[0].Weight);
+            Assert.AreEqual(70, result[1].Weight);
+            Assert.AreEqual(95, result[2].Weight);
+        }
+
+    }
+}
